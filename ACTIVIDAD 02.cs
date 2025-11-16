32bc91b084e544b95142eec0aef34168c68bd02d
JUAN CARLOS ALVAREZ A <jeici0507@gmail.com>
2025-11-15 23:39:15 -0500

    Add files via upload

---
+++ ACTIVIDAD 02.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    // Esta función 'void' no retorna ningún valor, solo ejecuta una acción.
+    static void Saludar(string nombre, int edad)
+    {
+        Console.WriteLine($"¡Hola, {nombre}! Veo que tienes {edad} años.");
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese su nombre: ");
+        string nombreUsuario = Console.ReadLine();
+
+        Console.Write("Ingrese su edad: ");
+        int edadUsuario = int.Parse(Console.ReadLine());
+
+        // Se llama a la función para que realice la acción de imprimir
+        Saludar(nombreUsuario, edadUsuario);
+    }
+}
---
+++ ACTIVIDAD 03.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    // Función que usa lógica condicional para retornar un string
+    static string VerificarMayoriaEdad(int edad)
+    {
+        if (edad >= 18)
+        {
+            return "Mayor de edad";
+        }
+        else
+        {
+            return "Menor de edad";
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese su edad: ");
+        int edad = int.Parse(Console.ReadLine());
+
+        string resultado = VerificarMayoriaEdad(edad);
+
+        Console.WriteLine($"La persona es: {resultado}");
+    }
+}
