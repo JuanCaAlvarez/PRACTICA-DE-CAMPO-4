ccac31cf21cb148b7d38644ecb903ecc5c594cf9
JUAN CARLOS ALVAREZ A <jeici0507@gmail.com>
2025-11-15 23:37:27 -0500

    Add files via upload

---
+++ CONVERTIR TEMPERATURA.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    // Función que recibe Celsius y retorna Fahrenheit
+    static double ConvertirAFahrenheit(double celsius)
+    {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese la temperatura en grados Celsius: ");
+        double c = double.Parse(Console.ReadLine());
+
+        double fahrenheit = ConvertirAFahrenheit(c);
+
+        Console.WriteLine($"{c}°C equivale a {fahrenheit}°F");
+    }
+}
